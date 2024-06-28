3cd43da7595f640e4fc64cbd5871ccf480cbf066
Lucas Serafim <lucas.serafin@protonmail.ch>
2024-06-28 13:35:45 -0300

    Criação do Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-0 left-0 right-0 z-999 px-4 py-3 text-center bg-white">
+      <div className="text-center">
+        <p>
+          Copyright © 2024 -
+          <a className="font-semibold" href="https://office-ce.com.br/">
+            Office Contábil
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
